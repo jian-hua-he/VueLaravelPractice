398be4fc3d39ed8ac8d3886a775a4cd38b956fee
Jian-Hua, He <game09116@gmail.com>
2016-05-18 21:37:37 +0800

    Add webpack setting.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,28 @@
 var elixir = require('laravel-elixir');
 var publicJsPath = 'public/js';
+var webpackConfig = {
+    babel: {
+        // enable stage 0 babel transforms.
+        presets: ['es2015'],
+        plugins: ['transform-runtime'],
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.vue$/, // a regex for matching all files that end in `.vue`
+                loader: 'vue'   // loader to use for matched files
+            },
+            {
+                // use babel-loader for *.js files
+                test: /\.js$/,
+                loader: 'babel',
+                // important: exclude files in node_modules
+                // otherwise it's going to be really slow!
+                exclude: /node_modules/,
+            },
+        ],
+    },
+};
 
 require('laravel-elixir-webpack');
 
@@ -24,6 +47,6 @@ elixir(function(mix) {
     mix.webpack('vue/computed_setter.js', {}, publicJsPath + '/vue/computed_setter.js');
     mix.webpack('vue/style_binding.js', {}, publicJsPath + '/vue/style_binding.js');
     mix.webpack('vue/component.js', {}, publicJsPath + '/vue/component.js');
-    mix.webpack('vue/modularization.js', {}, publicJsPath + '/vue/modularization.js');
+    mix.webpack('vue/modularization.js', webpackConfig, publicJsPath + '/vue/modularization.js');
     mix.sass('app.scss');
 });
